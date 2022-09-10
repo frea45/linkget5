f3cd5c43bdc7768489276ad8b2b154eca6a6ded7
frea45 <110092585+frea45@users.noreply.github.com>
2022-09-10 14:15:48 +0430

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = int( os.getenv("api_id","3335796") )
     API_HASH = os.getenv("api_hash","138b992a0e672e8346d8439c3f42ea78")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001249461809") )
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001787336960") )
     CHANNEL_USERNAME = os.getenv("channel_username","irbotz")
-    TOKEN = os.getenv("token","xxxxx")
+    TOKEN = os.getenv("token","1943275919:AAFwSScrlR_i-AzdNTOiw8EEctbU_exOaGg")
     DOMAIN  = os.getenv("domain","https://ir-fastlink.herokuapp.com")
